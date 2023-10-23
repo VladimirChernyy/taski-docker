def49801db96a20b109cde06e008326264f8db18
vladimirchernyy <vladimir_chernyy@list.ru>
2023-10-23 11:11:15 +0400

    review settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') in ('True', '1', 't')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
